f1f469a3424e01a07c7c06be584964c210a99c35
sari32 <s0548434114@gmail.com>
2025-11-25 23:34:52 +0200

    Add Task entity and TaskStatus enum

---
+++ src/Task.java
@@ -0,0 +1,57 @@
+public class Task {
+    private final int id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+
+    public Task(int id, String title, String description, TaskStatus status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Task(int id, String title, String description) {
+        this(id, title, description, TaskStatus.NEW);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
+
+
